37670ecaffc8b9d15449cba08cce85b909a8820b
Prince Oncada <prince.oncada@gmail.com>
2023-09-09 04:40:53 +0800

    first commit

---
+++ src/test/kotlin/com/example/demo/datasource/mock/MockBankDataSourceTest.kt
@@ -0,0 +1,31 @@
+package com.example.demo.datasource.mock
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class MockBankDataSourceTest {
+
+    private val mockDataSource = MockBankDataSource()
+
+    @Test
+    fun `should provide a collection of banks`() {
+        // when
+        val banks = mockDataSource.retrieveBanks()
+
+        // then
+        assertThat(banks.size).isGreaterThanOrEqualTo(3)
+    }
+    
+    @Test
+    fun `should provide some mock data`() {
+        // when
+        val banks = mockDataSource.retrieveBanks()
+        
+        // then
+        assertThat(banks).allMatch { it.accountNumber.isNotBlank() }
+        assertThat(banks).anyMatch { it.trust != 0.0 }
+        assertThat(banks).allMatch { it.transactionFee != 0 }
+    }
+}
